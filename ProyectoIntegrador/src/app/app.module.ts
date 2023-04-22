dd23f047725baa100c0eebc013bbb0bccc88dcf5
Astronotus87 <avila.intercar@gmail.com>
2023-04-22 14:36:28 -0300

    componente Logo-ap

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
